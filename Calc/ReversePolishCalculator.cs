42b4bddeacb15cbb3ef5788aea3dd639be770a95
vmm <capvoln01@gmail.com>
2025-02-27 00:17:03 +0300

    Реализация кода таким образом чтобы все тесты выполнялись.

--- Calc/ReversePolishCalculator.cs
+++ Calc/ReversePolishCalculator.cs
@@ -10,7 +10,36 @@ namespace Calc
 	{
 		public double Evaluate(string expression)
 		{
-			return 3.14;
+			string[] tokens = expression.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+			Stack<double> stack = new Stack<double>();
+
+			foreach (string token in tokens)
+			{
+				if (double.TryParse(token, out double num))
+				{
+					stack.Push(num);
+				}
+				else
+				{
+					double b = stack.Pop();
+					double a = stack.Pop();
+
+					switch (token)
+					{
+						case "+": stack.Push(a + b); break;
+						case "-": stack.Push(a - b); break;
+						case "*": stack.Push(a * b); break;
+						case "/":
+							if (b == 0) throw new DivideByZeroException();
+							stack.Push(a / b);
+							break;
+						default:
+							throw new InvalidOperationException($"Неизвестный оператор: {token}");
+					}
+				}
+			}
+
+			return stack.Pop();
 		}
 	}
 }
