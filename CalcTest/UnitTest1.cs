e2f43677a2b63bdb195840756591a240855c7a45
vmm <capvoln01@gmail.com>
2025-02-27 00:34:14 +0300

    Разработка тестов для сложных выражений: произвольное количество операций и операндов.

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -1,4 +1,3 @@
-using Xunit;
 using Calc;
 
 namespace CalcTest
@@ -20,11 +19,27 @@ namespace CalcTest
 
 		[Theory]
 		[InlineData("3 4 + 2 *", 14)]
-		[InlineData("5 1 2 + 4 * + 3 -", 14)]
+		[InlineData("8 4 / 2 +", 4)]
 		public void Evaluate_TwoOperations_ReturnsCorrectResult(string expr, double expected)
 		{
 			double result = calculator.Evaluate(expr);
 			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("15 7 1 1 + - / 3 * 2 1 1 + + -", 5)]
+		[InlineData("2 3 4 + * 5 -", 9)]
+		[InlineData("5 1 2 + 4 * + 3 -", 14)]
+		public void Evaluate_ComplexExpressions_ReturnsCorrectResult(string expr, double expected)
+		{
+			double result = calculator.Evaluate(expr);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Evaluate_DivisionByZero_ThrowsException()
+		{
+			Assert.Throws<DivideByZeroException>(() => calculator.Evaluate("5 0 /"));
 		}
 	}
 }
